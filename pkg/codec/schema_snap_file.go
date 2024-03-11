e2f66727d80ffe952499eae392037231ff9eabb3
zhangjinpeng87 <zzzhangjinpeng@gmail.com>
2024-03-11 11:11:56 -0700

    dispatcher: use proto message to simplify schema-snap-file encoding/decoding
    Signed-off-by: zhangjinpeng87 <zzzhangjinpeng@gmail.com>

--- pkg/codec/schema_snap_file.go
+++ pkg/codec/schema_snap_file.go
@@ -2,11 +2,11 @@ package codec
 
 import (
 	"encoding/binary"
-	"encoding/json"
 	"io"
 
-	"github.com/pingcap/tidb/pkg/parser/model"
 	"github.com/zhangjinpeng87/tistream/pkg/utils"
+	pb "github.com/zhangjinpeng87/tistream/proto/go/tistreampb"
+	"google.golang.org/protobuf/proto"
 )
 
 const (
@@ -14,58 +14,46 @@ const (
 	SchemaFileVersion     = 1
 )
 
-type SchemaSnap struct {
+type SchemaSnapFile struct {
 	// All the schema and table info.
-	dbs    []*model.DBInfo
-	tables []*model.TableInfo
-
-	// The timestamp of the schema snapshot.
-	currentTs uint64
+	snap *pb.SchemaSnapshot
 }
 
 // NewSchemaSnap creates a new SchemaSnap.
-func NewEmptySchemaSnap() *SchemaSnap {
-	return &SchemaSnap{
-		dbs:       make([]*model.DBInfo, 0),
-		tables:    make([]*model.TableInfo, 0),
-		currentTs: 0,
-	}
+func NewEmptySchemaSnap() *SchemaSnapFile {
+	return &SchemaSnapFile{}
 }
 
-func (s *SchemaSnap) EncodeTo(writer io.Writer) error {
+func (s *SchemaSnapFile) EncodeTo(writer io.Writer) error {
 	// Encode the header.
-	writer.Write([]byte(SchemaFileMagicHeader))
-	// Encode the version.
-	binary.Write(writer, binary.LittleEndian, uint32(SchemaFileVersion))
-	// Encode the timestamp.
-	binary.Write(writer, binary.LittleEndian, s.currentTs)
-	// Encode the number of dbs.
-	binary.Write(writer, binary.LittleEndian, uint64(len(s.dbs)))
-	// Encode the number of tables.
-	binary.Write(writer, binary.LittleEndian, uint64(len(s.tables)))
-
-	// Encode the dbs.
-	for _, db := range s.dbs {
-		marshal, err := json.Marshal(db)
-		if err != nil {
-			return err
-		}
-		writer.Write(marshal)
+	_, err := writer.Write([]byte(SchemaFileMagicHeader))
+	if err != nil {
+		return err
 	}
 
-	// Encode the tables.
-	for _, table := range s.tables {
-		marshal, err := json.Marshal(table)
-		if err != nil {
-			return err
-		}
-		writer.Write(marshal)
+	// Encode the version.
+	err = binary.Write(writer, binary.LittleEndian, uint32(SchemaFileVersion))
+	if err != nil {
+		return err
+	}
+
+	data, err := proto.Marshal(s.snap)
+	if err != nil {
+		return err
+	}
+	err = binary.Write(writer, binary.LittleEndian, uint32(len(data)))
+	if err != nil {
+		return err
+	}
+	_, err = writer.Write(data)
+	if err != nil {
+		return err
 	}
 
 	return nil
 }
 
-func (s *SchemaSnap) DecodeFrom(reader io.Reader) error {
+func (s *SchemaSnapFile) DecodeFrom(reader io.Reader) error {
 	// Read the header.
 	header := make([]byte, len(SchemaFileMagicHeader))
 	n, err := reader.Read(header)
@@ -87,103 +75,24 @@ func (s *SchemaSnap) DecodeFrom(reader io.Reader) error {
 		return err
 	}
 
-	// Read the timestamp.
-	err = binary.Read(reader, binary.LittleEndian, &s.currentTs)
+	// Read the data.
+	var dataLen uint32
+	err = binary.Read(reader, binary.LittleEndian, &dataLen)
+	if err != nil {
+		return err
+	}
+	data := make([]byte, dataLen)
+	_, err = io.ReadFull(reader, data)
 	if err != nil {
 		return err
 	}
 
-	// Read the number of dbs.
-	var dbNum uint64
-	err = binary.Read(reader, binary.LittleEndian, &dbNum)
+	// Decode the data.
+	s.snap = &pb.SchemaSnapshot{}
+	err = proto.Unmarshal(data, s.snap)
 	if err != nil {
 		return err
-	}
-
-	// Read the number of tables.
-	var tableNum uint64
-	err = binary.Read(reader, binary.LittleEndian, &tableNum)
-	if err != nil {
-		return err
-	}
-
-	// Read the dbs.
-	for i := 0; i < int(dbNum); i++ {
-		db := &model.DBInfo{}
-		err = json.NewDecoder(reader).Decode(db)
-		if err != nil {
-			return err
-		}
-		s.dbs = append(s.dbs, db)
-	}
-
-	// Read the tables.
-	for i := 0; i < int(tableNum); i++ {
-		table := &model.TableInfo{}
-		err = json.NewDecoder(reader).Decode(table)
-		if err != nil {
-			return err
-		}
-		s.tables = append(s.tables, table)
 	}
 
 	return nil
-}
-
-// Compare the schema snap. Used for test.
-func (s *SchemaSnap) equalTo(s2 *SchemaSnap) bool {
-	if s.currentTs != s2.currentTs {
-		return false
-	}
-
-	if len(s.dbs) != len(s2.dbs) {
-		return false
-	}
-
-	for i := 0; i < len(s.dbs); i++ {
-		if s.dbs[i].ID != s2.dbs[i].ID {
-			return false
-		}
-		if s.dbs[i].Name.L != s2.dbs[i].Name.L {
-			return false
-		}
-		if s.dbs[i].Charset != s2.dbs[i].Charset {
-			return false
-		}
-		if s.dbs[i].Collate != s2.dbs[i].Collate {
-			return false
-		}
-	}
-
-	if len(s.tables) != len(s2.tables) {
-		panic("tables length not equal")
-	}
-
-	for i := 0; i < len(s.tables); i++ {
-		if s.tables[i].ID != s2.tables[i].ID {
-			return false
-		}
-		if s.tables[i].Name.L != s2.tables[i].Name.L {
-			return false
-		}
-		if s.tables[i].Charset != s2.tables[i].Charset {
-			return false
-		}
-		if s.tables[i].Collate != s2.tables[i].Collate {
-			return false
-		}
-		if len(s.tables[i].Columns) != len(s2.tables[i].Columns) {
-			return false
-		}
-		for j := 0; j < len(s.tables[i].Columns); j++ {
-			if s.tables[i].Columns[j].ID != s2.tables[i].Columns[j].ID {
-				return false
-			}
-			if s.tables[i].Columns[j].Name.L != s2.tables[i].Columns[j].Name.L {
-				return false
-			}
-		}
-	}
-
-	return true
 }
